0b599acdc97f40e18b7ded2212f8269e552a9d40
elsen <skadiri94@yahoo.com>
2020-10-29 20:30:59 +0000

    29/10/2020

--- tests/spelling/SpellingSuggesterTest.java
+++ tests/spelling/SpellingSuggesterTest.java
@@ -3,7 +3,6 @@ package spelling;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,8 +14,7 @@ class SpellingSuggesterTest {
     @Test
     void getSuggestions() {
         Iterable<String> actual = spellingSuggester.getSuggestions("ther");
-
-        Iterable<String> expected = new ArrayList(asList("cher", "sher", "teer",
+        ArrayList<String> expected = new ArrayList<>(asList("cher", "sher", "teer",
                 "tier", "thar", "thir", "thor", "thea", "theb", "thed", "thee",
                 "them", "then", "theo", "thew", "they", "her", "ter", "the"));
 
